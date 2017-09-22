02c9155bff321d7e093ad001286455b172448c5d
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-09-22 08:21:40 -0400

    Add tiny url code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,60 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import Lib
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Lucid
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Lucid
+import System.Directory
+import Text.Blaze
+import Text.Blaze.Html.Renderer.Utf8
+import qualified Data.Aeson.Parser
+import qualified Text.Blaze.Html
+
+type TinyUrlAPI = "tinyUrl" :> Capture "value" String :> Get '[JSON] TinyUrlResponse
+
+newtype TinyUrl = TinyUrl String
+
+data TinyUrlResponse = TinyUrlResponse
+  { value :: TinyUrl } deriving (Show, Generic)
+
+instance ToJSON TinyUrlResponse
+
+tinyUrl :: TinyUrl
+tinyUrl = TinyUrl
+
+tinyUrlAPI :: Proxy TinyUrlAPI
+tinyUrlAPI = Proxy
+
+server :: Server TinyUrlAPI
+server = return tinyUrl
+
+app :: Application
+app = serve tinyUrlAPI server
 
 main :: IO ()
-main = someFunc
+main = run 8081 app
+
