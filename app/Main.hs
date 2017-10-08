204fd751ed862cfd6ee0e40f3964894a90405041
Kevin Meredith <kevin.m.meredith@gmail.com>
2017-10-08 14:33:21 -0400

    Add 'put' partially.

--- app/Main.hs
+++ app/Main.hs
@@ -24,7 +24,7 @@ import Data.Map
 import Control.Monad.Except
 
 type API = "tinyUrl" :> Capture "value" String :> (
-              Get '[JSON, HTML] ResolvedTinyUrl :<|> ReqBody '[JSON] ResolvedTinyUrl :> Put '[] ()
+              Get '[JSON, HTML] ResolvedTinyUrl :<|> ReqBody '[JSON] UpdatedTinyUrl :> Put '[] NoContent
             )
 
 -- credit: https://stackoverflow.com/questions/46390448/deriving-tohtml-for-newtype
@@ -32,10 +32,14 @@ newtype TinyUrl = TinyUrl String deriving (Generic, ToHtml, Ord, Eq)
 
 instance ToJSON TinyUrl
 
-data ResolvedTinyUrl = ResolvedTinyUrl
-  { value :: TinyUrl } deriving Generic
+newtype ResolvedTinyUrl = ResolvedTinyUrl { value :: TinyUrl } deriving Generic
+
+data UpdatedTinyUrl = UpdatedTinyUrl
+  { k :: TinyUrl, v :: String } deriving Generic
 
 instance ToJSON ResolvedTinyUrl
+
+instance FromJSON UpdatedTinyUrl
 
 instance ToHtml ResolvedTinyUrl where
   toHtml x =
@@ -54,15 +58,25 @@ tinyUrlAPI = Proxy
 -- Handler ResolvedTinyUrl
 
 server :: IO (MVar (Map TinyUrl String)) -> Server API
-server ioMap = f
-  where f :: String -> Handler ResolvedTinyUrl
-        f s = Handler $ do
+server ioMap = get :<|> put
+
+  where get :: String -> Handler ResolvedTinyUrl
+        get s = Handler $ do
           map    <- lift $ ioMap
           m      <- lift $ takeMVar map
           found  <- lift $ return $ Data.Map.lookup (TinyUrl s) m
           case found of
              Just a  -> return $ ResolvedTinyUrl (TinyUrl a)
              Nothing -> (lift $ putStrLn ("did not find " ++ s)) >> throwError err404
+
+        put :: UpdatedTinyUrl -> Handler ()
+        put (UpdatedTinyUrl key value) = Handler $ do
+         map     <- lift $ ioMap
+         m       <- lift $ takeMVar map
+         updated <- lift $ Data.Map.insert key value m
+         _       <- lift $ putMVar map updated
+         return NoContent
+
 
 app :: IO (MVar (Map TinyUrl String)) -> Application
 app map = serve tinyUrlAPI (server map)
